964b569390f4409c6bbc301dc1336c156d61a4fb
kei ito <ito.kei@jvckenwood.com>
2024-03-14 14:14:30 +0900

    テストコード拡充

--- WpfApp1/ClassLibraryTests/StringExtensionsTest.cs
+++ WpfApp1/ClassLibraryTests/StringExtensionsTest.cs
@@ -4,25 +4,41 @@ namespace ClassLibraryTests
 {
     public class StringExtensionsTest
     {
-        [Fact]
-        public void 半角英数字かを判定＿ＯＫ()
-        {
-            string testCase = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        #region 半角英数字
 
-            foreach (var c in testCase)
+        [Theory]
+        [InlineData("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]//半角英数
+        public void 半角英数字かを判定＿ＯＫ(string str)
+        {
+            foreach (var c in str)
             {
-                Assert.True(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
+                Assert.True(c.IsOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
             }
         }
 
-        [Fact]
-        public void 半角英数字かを判定＿NG()
+        [Theory]
+        [InlineData("アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン")]//全角カタカナ
+        [InlineData("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ")]//半角カタカナ
+        [InlineData("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん")]//ひらがな
+        [InlineData("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")]//半角記号
+        [InlineData("！“#＄％＆‘（）＊＋,－./：；＜＝＞？＠［￥］＾＿‘｛｜｝～　")]//全角記号
+        [InlineData("亜唖娃阿哀愛挨姶逢葵茜")]//第1水準漢字　面区冒頭
+        [InlineData("拭植殖燭織職色触食蝕辱")]//第1水準漢字　面区中盤
+        [InlineData("鷲亙亘鰐詫藁蕨椀湾碗腕")]//第1水準漢字　面区末尾
+        [InlineData("弌丐丕个丱丶丼丿乂乖乘")]//第2水準漢字　面区冒頭
+        [InlineData("狹狷倏猗猊猜猖猝猴猯猩")]//第2水準漢字　面区中盤
+        [InlineData("堯槇遙瑤凜熙齷齲齶龕龜龠")]//第2水準漢字　面区末尾
+        [InlineData("俱𠀋㐂丨丯丰亍仡份仿伃")]//第3水準漢字　面区冒頭
+        [InlineData("琇琊琚琛琢琦琨琪琫琬琮")]//第3水準漢字　面区中盤
+        [InlineData("鼹齗龐龔龗龢姸屛幷瘦繫")]//第3水準漢字　面区末尾
+        [InlineData("𠂉丂丏丒丩丫丮乀乇么𠂢")]//第4水準漢字　面区冒頭
+        [InlineData("𥆩䀹𥇥𥇍睘睠睪𥈞睲睼睽")]//第4水準漢字　面区中盤
+        [InlineData("齕齘𪗱齝𪘂齩𪘚齭齰齵𪚲")]//第4水準漢字　面区末尾
+        public void 半角英数字かを判定＿NG(string str)
         {
-            string testCase = "_ +Ａあアｱ亜Ąａ１";
-
-            foreach (var c in testCase)
+            foreach (var c in str)
             {
-                Assert.False(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
+                Assert.False(c.IsOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
             }
         }
 
@@ -38,26 +54,43 @@ namespace ClassLibraryTests
             Assert.Equal(output, input.ExtractOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
         }
 
+        #endregion
 
-        [Fact]
-        public void JIS第1水準漢字までかを判定＿ＯＫ()
+        #region 第1水準漢字まで
+
+        [Theory]
+        [InlineData("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]//半角英数
+        [InlineData("アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン")]//全角カタカナ
+        [InlineData("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ")]//半角カタカナ
+        [InlineData("あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん")]//ひらがな
+        [InlineData("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")]//半角記号
+        [InlineData("！“#＄％＆‘（）＊＋,－./：；＜＝＞？＠［￥］＾＿‘｛｜｝～　")]//全角記号
+        [InlineData("亜唖娃阿哀愛挨姶逢葵茜")]//第1水準漢字　面区冒頭
+        [InlineData("拭植殖燭織職色触食蝕辱")]//第1水準漢字　面区中盤
+        [InlineData("鷲亙亘鰐詫藁蕨椀湾碗腕")]//第1水準漢字　面区末尾
+        public void JIS第1水準漢字までかを判定＿ＯＫ(string str)
         {
-            string testCase = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ /+-?あアａＡｱ亜唖娃阿哀愛挨姶逢葵茜穐";
-
-            foreach (var c in testCase)
+            foreach (var c in str)
             {
-                Assert.True(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
+                Assert.True(c.IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
             }
         }
 
-        [Fact]
-        public void JIS第1水準漢字までかを判定＿NG()
+        [Theory]
+        [InlineData("弌丐丕个丱丶丼丿乂乖乘")]//第2水準漢字　面区冒頭
+        [InlineData("狹狷倏猗猊猜猖猝猴猯猩")]//第2水準漢字　面区中盤
+        [InlineData("堯槇遙瑤凜熙齷齲齶龕龜龠")]//第2水準漢字　面区末尾
+        [InlineData("俱𠀋㐂丨丯丰亍仡份仿伃")]//第3水準漢字　面区冒頭
+        [InlineData("琇琊琚琛琢琦琨琪琫琬琮")]//第3水準漢字　面区中盤
+        [InlineData("鼹齗龐龔龗龢姸屛幷瘦繫")]//第3水準漢字　面区末尾
+        [InlineData("𠂉丂丏丒丩丫丮乀乇么𠂢")]//第4水準漢字　面区冒頭
+        [InlineData("𥆩䀹𥇥𥇍睘睠睪𥈞睲睼睽")]//第4水準漢字　面区中盤
+        [InlineData("齕齘𪗱齝𪘂齩𪘚齭齰齵𪚲")]//第4水準漢字　面区末尾
+        public void JIS第1水準漢字までかを判定＿NG(string str)
         {
-            string testCase = "弌丐丕个丱丶丼丿乂乖乘亂亅豫亊舒弍于亞亟亠亢亰亳亶从仍俱𠀋㐂丨丯丰亍仡份仿伃伋你佈佉佖佟佪佬佾𠂉丂丏丒丩丫丮乀乇么𠂢乑㐆𠂤乚乩亝㐬㐮亹";
-
-            foreach (var c in testCase)
+            foreach (var c in str)
             {
-                Assert.False(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
+                Assert.False(c.IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
             }
         }
 
@@ -72,5 +105,7 @@ namespace ClassLibraryTests
         {
             Assert.Equal(output, input.ExtractOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
         }
+
+        #endregion
     }
 }

13998ed86ae42f240a4f95594201d9ffecf19658
kei ito <ito.kei@jvckenwood.com>
2024-03-14 13:34:35 +0900

    JIS第1水準漢字までの文字制限を実装

--- WpfApp1/ClassLibrary/StringExtensions.cs
+++ WpfApp1/ClassLibrary/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -18,6 +19,11 @@ namespace ClassLibrary
         /// 半角英数字
         /// </summary>
         HalfWidthAlphanumeric,
+
+        /// <summary>
+        /// JIS第1水準漢字まで
+        /// </summary>
+        UpToJisLevel1KanjiSet,
     }
 
     /// <summary>
@@ -28,6 +34,17 @@ namespace ClassLibrary
         #region 利用可能な文字のみかを判定
 
         /// <summary>
+        /// 利用可能な文字かを判定します。
+        /// </summary>
+        /// <param name="c">評価対象文字</param>
+        /// <param name="type">利用可能な文字種別</param>
+        /// <returns>利用可能な文字の場合 true</returns>
+        public static bool IsOnlyAbailableCharacters(this char c, AvailableCharactersType type)
+        {
+            return c.ToString().IsOnlyAbailableCharacters(type);
+        }
+
+        /// <summary>
         /// 利用可能な文字のみかを判定します。
         /// </summary>
         /// <param name="str">評価対象文字列</param>
@@ -35,18 +52,254 @@ namespace ClassLibrary
         /// <returns>利用可能な文字のみの場合 true</returns>
         public static bool IsOnlyAbailableCharacters(this string str, AvailableCharactersType type)
         {
-            bool ret = false;
+            bool ret;
 
             switch (type)
             {
                 case AvailableCharactersType.HalfWidthAlphanumeric:
                     ret = Regex.IsMatch(str, @"^[0-9a-zA-Z]+$");
                     break;
-                default:
+                case AvailableCharactersType.UpToJisLevel1KanjiSet:
+                    ret = IsUntilJISKanjiLevel2(str, true);
                     break;
+                default:
+                    throw new NotImplementedException();
             }
 
             return ret;
+        }
+
+        /// <summary>
+        /// 文字列が半角英数字記号かどうかを判定します
+        /// </summary>
+        /// <param name="target">対象の文字列</param>
+        /// <returns>文字列が半角英数字記号の場合はtrue、それ以外はfalse</returns>
+        public static bool IsASCII(string target)
+        {
+            return new Regex("^[\x20-\x7E]+$").IsMatch(target);
+        }
+
+        /// <summary>
+        /// 文字列が半角カタカナ（句読点～半濁点）かどうかを判定します
+        /// </summary>
+        /// <param name="target">対象の文字列</param>
+        /// <returns>文字列が半角カタカナ（句読点～半濁点）の場合はtrue、それ以外はfalse</returns>
+        public static bool IsHalfKatakanaPunctuation(string target)
+        {
+            return new Regex("^[\uFF61-\uFF9F]+$").IsMatch(target);
+        }
+
+        /// <summary>
+        /// 文字列がJIS X 0208 漢字第二水準までで構成されているかを判定します
+        /// </summary>
+        /// <param name="target">対象の文字列</param>
+        /// <param name="containsHalfKatakana">漢字第二水準までに半角カタカナを含む場合はtrue、それ以外はfalse</param>
+        /// <returns>文字列がJIS X 0208 漢字第二水準までで構成されている場合はtrue、それ以外はfalse</returns>
+        public static bool IsUntilJISKanjiLevel2(string target, bool containsHalfKatakana)
+        {
+            // 文字エンコーディングに「iso-2022-jp」を指定
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.GetEncoding("iso-2022-jp");
+            // 文字列長を取得
+            int length = target.Length;
+            for (int i = 0; i < length; i++)
+            {
+                // 対象の部分文字列を取得
+                string targetSubString = target.Substring(i, 1);
+                // 半角英数字記号の場合
+                if (IsASCII(targetSubString) == true)
+                {
+                    continue;
+                }
+
+                // 漢字第二水準までに半角カタカナを含まずかつ対象の部分文字列が半角カタカナの場合
+                if (containsHalfKatakana == false &&
+                    IsHalfKatakanaPunctuation(targetSubString) == true)
+                {
+                    return false;
+                }
+
+                // 対象部分文字列の文字コードバイト配列を取得
+                byte[] targetBytes = encoding.GetBytes(targetSubString);
+                // 要素数が「1」の場合は漢字第三水準以降の漢字が「?」に変換された
+                if (targetBytes.Length == 1)
+                {
+                    return false;
+                }
+
+                // 文字コードバイト配列がJIS X 0208 漢字第二水準外の場合
+                if (IsUntilJISKanjiLevel2(targetBytes) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 文字列がJIS X 0208 漢字第二水準までで構成されているかを判定します
+        /// </summary>
+        /// <param name="target">対象の文字列</param>
+        /// <returns>文字列がJIS X 0208 漢字第二水準までで構成されている場合はtrue、それ以外はfalse</returns>
+        /// <remarks>句読点～半濁点の半角カタカナはJIS X 0208 漢字第二水準外と判定します</remarks>
+        public static bool IsUntilJISKanjiLevel2(string target)
+        {
+            return IsUntilJISKanjiLevel2(target, false);
+        }
+
+        /// <summary>
+        /// 文字コードバイト配列がJIS X 0208 漢字第二水準までであるかを判定します
+        /// </summary>
+        /// <param name="targetBytes">文字コードバイト配列</param>
+        /// <returns>文字コードバイト配列がJIS X 0208 漢字第二水準までである場合はtrue、それ以外はfalse</returns>
+        private static bool IsUntilJISKanjiLevel2(byte[] targetBytes)
+        {
+            // 文字コードバイト配列の要素数が8ではない場合
+            if (targetBytes.Length != 8)
+            {
+                return false;
+            }
+
+            // 区を取得
+            int row = targetBytes[3] - 0x20;
+            // 点を取得
+            int cell = targetBytes[4] - 0x20;
+            switch (row)
+            {
+                case 1: // 1区の場合
+                    if (cell is >= 1 and <= 94)
+                    {
+                        // 1点～94点の場合
+                        return true;
+                    }
+
+                    break;
+                case 2: // 2区の場合
+                    if (cell is >= 1 and <= 14)
+                    {
+                        // 1点～14点の場合
+                        return true;
+                    }
+                    else if (cell is >= 26 and <= 33)
+                    {
+                        // 26点～33点の場合
+                        return true;
+                    }
+                    else if (cell is >= 42 and <= 48)
+                    {
+                        // 42点～48点の場合
+                        return true;
+                    }
+                    else if (cell is >= 60 and <= 74)
+                    {
+                        // 60点～74点の場合
+                        return true;
+                    }
+                    else if (cell is >= 82 and <= 89)
+                    {
+                        // 82点～89点の場合
+                        return true;
+                    }
+                    else if (cell == 94)
+                    {
+                        // 94点の場合
+                        return true;
+                    }
+
+                    break;
+                case 3: // 3区の場合
+                    if (cell is >= 16 and <= 25)
+                    {
+                        // 16点～25点の場合
+                        return true;
+                    }
+                    else if (cell is >= 33 and <= 58)
+                    {
+                        // 33点～58点の場合
+                        return true;
+                    }
+                    else if (cell is >= 65 and <= 90)
+                    {
+                        // 65点～90点の場合
+                        return true;
+                    }
+
+                    break;
+                case 4: // 4区の場合
+                    if (cell is >= 1 and <= 83)
+                    {
+                        // 1点～83点の場合
+                        return true;
+                    }
+
+                    break;
+                case 5: // 5区の場合
+                    if (cell is >= 1 and <= 86)
+                    {
+                        // 1点～86点の場合
+                        return true;
+                    }
+
+                    break;
+                case 6: // 6区の場合
+                    if (cell is >= 1 and <= 24)
+                    {
+                        // 1点～24点の場合
+                        return true;
+                    }
+                    else if (cell is >= 33 and <= 56)
+                    {
+                        // 33点～56点の場合
+                        return true;
+                    }
+
+                    break;
+                case 7: // 7区の場合
+                    if (cell is >= 1 and <= 33)
+                    {
+                        // 1点～33点の場合
+                        return true;
+                    }
+                    else if (cell is >= 49 and <= 81)
+                    {
+                        // 49点～81点の場合
+                        return true;
+                    }
+
+                    break;
+                case 8: // 8区の場合
+                    if (cell is >= 1 and <= 32)
+                    {
+                        // 1点～32点の場合
+                        return true;
+                    }
+
+                    break;
+                default:
+                    if (row is >= 16 and <= 46)
+                    {
+                        // 16区～46区の場合
+                        if (cell is >= 1 and <= 94)
+                        {
+                            // 1点～94点の場合
+                            return true;
+                        }
+                    }
+                    else if (row == 47)
+                    {
+                        // 47区の場合
+                        if (cell is >= 1 and <= 51)
+                        {
+                            // 1点～51点の場合
+                            return true;
+                        }
+                    }
+
+                    break;
+            }
+
+            return false;
         }
 
         #endregion
@@ -61,15 +314,24 @@ namespace ClassLibrary
         /// <returns>利用可能な文字のみの文字列</returns>
         public static string ExtractOnlyAbailableCharacters(this string str, AvailableCharactersType type)
         {
-            string ret = string.Empty;
+            string ret;
 
             switch (type)
             {
                 case AvailableCharactersType.HalfWidthAlphanumeric:
-                    ret = Regex.Replace(str, @"[^0-9a-zA-Z]", string.Empty);
+                    ret =
+                        string.Concat(
+                            str.Where(
+                                x => x.IsOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric)));
+                    break;
+                case AvailableCharactersType.UpToJisLevel1KanjiSet:
+                    ret =
+                        string.Concat(
+                            str.Where(
+                                x => x.IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet)));
                     break;
                 default:
-                    break;
+                    throw new NotImplementedException();
             }
 
             return ret;
--- WpfApp1/ClassLibraryTests/StringExtensionsTest.cs
+++ WpfApp1/ClassLibraryTests/StringExtensionsTest.cs
@@ -29,13 +29,48 @@ namespace ClassLibraryTests
         [Theory]
         [InlineData("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
             "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")]// 違反文字無し　変化なし
-        [InlineData("_ +Ａあアｱ亜Ąａ１", "")]
+        [InlineData("_ +Ａあアｱ亜Ąａ１", "")]//違反文字のみ
         [InlineData("abcあdefg1ｱ亜Ą23", "abcdefg123")]// 中盤に違反文字
         [InlineData("_ +abcあdefg1ｱ亜Ą23", "abcdefg123")]// 冒頭にも違反文字
         [InlineData("abcあdefg1ｱ亜Ą23ａ１", "abcdefg123")]// 末尾にも違反文字
         public void 半角英数を抽出(string input, string output)
         {
             Assert.Equal(output, input.ExtractOnlyAbailableCharacters(AvailableCharactersType.HalfWidthAlphanumeric));
+        }
+
+
+        [Fact]
+        public void JIS第1水準漢字までかを判定＿ＯＫ()
+        {
+            string testCase = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ /+-?あアａＡｱ亜唖娃阿哀愛挨姶逢葵茜穐";
+
+            foreach (var c in testCase)
+            {
+                Assert.True(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
+            }
+        }
+
+        [Fact]
+        public void JIS第1水準漢字までかを判定＿NG()
+        {
+            string testCase = "弌丐丕个丱丶丼丿乂乖乘亂亅豫亊舒弍于亞亟亠亢亰亳亶从仍俱𠀋㐂丨丯丰亍仡份仿伃伋你佈佉佖佟佪佬佾𠂉丂丏丒丩丫丮乀乇么𠂢乑㐆𠂤乚乩亝㐬㐮亹";
+
+            foreach (var c in testCase)
+            {
+                Assert.False(c.ToString().IsOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
+            }
+        }
+
+        [Theory]
+        [InlineData("FGHIJKLMNOPQRSTUVWXYZ /+-?あアａＡｱ亜唖娃阿哀愛挨姶逢葵茜穐",
+            "FGHIJKLMNOPQRSTUVWXYZ /+-?あアａＡｱ亜唖娃阿哀愛挨姶逢葵茜穐")]// 違反文字無し　変化なし
+        [InlineData("丐丕丗个丱丶丼丿乂乕乖乘乢亂亅亊于", "")]//違反文字のみ
+        [InlineData("WXYZ /+-丗个丱丶丼丿乂乕?あアａＡｱ亜", "WXYZ /+-?あアａＡｱ亜")]// 中盤に違反文字
+        [InlineData("丗个丱丶丼丿乂乕WXYZ /+-?あアａＡｱ亜", "WXYZ /+-?あアａＡｱ亜")]// 冒頭にも違反文字
+        [InlineData("WXYZ /+-?あアａＡｱ亜丗个丱丶丼丿乂乕", "WXYZ /+-?あアａＡｱ亜")]// 末尾にも違反文字
+        public void JIS第1水準漢字までを抽出(string input, string output)
+        {
+            Assert.Equal(output, input.ExtractOnlyAbailableCharacters(AvailableCharactersType.UpToJisLevel1KanjiSet));
         }
     }
 }
